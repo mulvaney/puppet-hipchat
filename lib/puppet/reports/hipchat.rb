17542959cccce51bf687f07c793a8fe6cf109908
Vadym Chepkov <vchepkov@gmail.com>
2014-11-27 10:46:59 -0500

    debug

--- lib/puppet/reports/hipchat.rb
+++ lib/puppet/reports/hipchat.rb
@@ -67,6 +67,7 @@ Puppet::Reports.register_report(:hipchat) do
     if (HIPCHAT_STATUSES.include?(self.status) || HIPCHAT_STATUSES.include?('all')) && !disabled
       Puppet.debug "Sending status for #{self.host} to Hipchat channel #{HIPCHAT_ROOM}"
         msg = "Puppet run for #{self.host} #{emote(self.status)} #{self.status} at #{Time.now.asctime} on #{self.configuration_version} in #{self.environment}"
+        msg << ":: #{HIPCHAT_PUPPETBOARD} ::"
         if HIPCHAT_PUPPETBOARD != "false"
           msg << ": #{HIPCHAT_PUPPETBOARD}/report/latest/#{self.host}"
         elsif HIPCHAT_DASHBOARD != "false"
